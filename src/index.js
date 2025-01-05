6e2b6ad0d2755e6f44c71fa62ecf7ce9303f9906
Flowher <florian.herremans@gmail.com>
2025-01-05 22:12:37 +0100

    Sixth  version

--- src/index.js
+++ src/index.js
@@ -29,8 +29,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    db = client.db('myAppDB')	
+    await client.db("admin").command({ ping: 1 });	
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -38,6 +37,7 @@ async function run() {
   }
 }
 run().catch(console.dir);
+db = client.db("myAppDB")
 
 
 const PORT = process.env.PORT;
